b85368ff462c4c09cc08a63ab282d3b7fe6782ce
silus91 <wizard91@o2.pl>
2021-01-06 22:53:01 +0000

    favicon in progress hot fix https errs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,13 +4,17 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Cost-Calculator</title>
+        <title>Magic Bubble Calculator</title>
         <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/icon?family=Material+Icons"
         />
-        <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
-        <!-- Compiled and minified CSS -->
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+
+        <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+        <link rel="icon" href="{{ URL::asset('favicon.ico') }}" type="image/x-icon"/>
+        <link type="image/x-icon" rel="apple-touch-icon" href="favicon.ico" />
+
         <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"
