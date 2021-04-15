b504ab7b35230fbce24102d1f000d875b5bc4b6a
silus91 <wizard91@o2.pl>
2021-04-15 23:20:13 +0100

    indentation fix

--- resources/js/react/components/Loader/Loader.js
+++ resources/js/react/components/Loader/Loader.js
@@ -2,33 +2,31 @@ import React, { Fragment, useContext } from "react";
 import { StoreContext } from "../../contexts/StoreContext";
 import "./Loader.css";
 
-  const Loader = () => {
+const Loader = () => {
 
-    const { store } = useContext(StoreContext);
+  const { store } = useContext(StoreContext);
 
-    return (
-      <Fragment>
-        {store.loading == true ? (
-          <div className='loaderContainer'>
-            <div className='preloader-wrapper big active'>
-              <div className='spinner-layer spinner-blue-only'>
-                <div className='circle-clipper left'>
-                  <div className='circle'></div>
-                </div>
-                <div className='gap-patch'>
-                  <div className='circle'></div>
-                </div>
-                <div className='circle-clipper right'>
-                  <div className='circle'></div>
-                </div>
+  return (
+    <Fragment>
+      {store.loading == true ? (
+        <div className='loaderContainer'>
+          <div className='preloader-wrapper big active'>
+            <div className='spinner-layer spinner-blue-only'>
+              <div className='circle-clipper left'>
+                <div className='circle'></div>
+              </div>
+              <div className='gap-patch'>
+                <div className='circle'></div>
+              </div>
+              <div className='circle-clipper right'>
+                <div className='circle'></div>
               </div>
             </div>
           </div>
-        ) : null}
-      </Fragment>
-    );
-  }
-
-
+        </div>
+      ) : null}
+    </Fragment>
+  );
+}
 
 export default Loader;
